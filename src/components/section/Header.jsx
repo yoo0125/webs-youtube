1a66664f7573ff4f072182be34c6c0dfa23ea2b2
yoo0125 <yooo0125@naver.com>
2023-11-29 19:00:46 +0900

    Header 작업중

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -3,7 +3,36 @@ import React from 'react'
 const Header = () => {
   return (
     <header id='header' role='banner'>
-        Header
+        <h1 className='header_logo'>
+          <a href='/'>
+            <em aria-hidden='true'></em>
+            <span>webs<br/>youtube</span>
+          </a>
+        </h1>
+        <nav className='header_menu'>
+          <ul>
+            <li>
+              <a href=''>웹스토리보이</a>
+            </li>
+            <li>
+              <a href='/today'>추천 영상</a>
+            </li>
+            <li>
+              <a href='/developer'>추천 개발자</a>
+            </li>
+            <li>
+              <a href='/webd'>웹디자인 기능사</a>
+            </li>
+            <li>
+              <a href='/website'>웹표준 사이트</a>
+            </li>
+            <li>
+              <a href='/webd'>웹디자인 기능사</a>
+            </li>
+          </ul>
+          {/* 07번 5분 14초*/}
+        </nav>
+        <div className='header_sns'></div>
     </header>
   )
 }
