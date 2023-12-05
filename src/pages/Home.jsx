ae119e4b5c519e4b87003ad770e4f5151cfd54ca
yoo0125 <yooo0125@naver.com>
2023-12-05 16:23:35 +0900

    10. 페이지 SEO 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Main from '../components/section/Main'
 
 const Home = () => {
-  return (
-    <Main>
-      Home
-    </Main>
-  )
+    return (
+        <Main 
+            title = "웹스토리보이 유튜브"
+            description="웹스토리보이 유튜버 사이트에 오신 것을 환영합니다.">
+            Home
+        </Main>
+    )
 }
 
 export default Home
